9b62b3ba9349f36225d8c54a890ad5a7d13cbbf5
justinmmorgan21 <justinmorgan21@gmail.com>
2024-12-31 09:11:14 -0600

    updated Child index display

--- src/components/ChildrenIndex.jsx
+++ src/components/ChildrenIndex.jsx
@@ -1,78 +1,83 @@
+
 export function ChildrenIndex( { children_results }) {
 
   return (
     <div>
       <h1>All children</h1>
-      <div className="cards">
+      <div>
         { children_results.map( child => (
           <div key={child.id} className="card">
-            <h2>{child.name}</h2>
-            <p>username: {child.username}</p>
-            <p>birthdate: {child.birthdate}</p>
-            <p>points available: {child.points_available}</p>
-            <p>money banked: {child.money_banked}</p>
-            <br />
-            <div>
-              <h4>Monday</h4>
-              {child.monday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
+            <div className="child-info">
+              <h2>{child.name}</h2>
+              <p>username: {child.username}</p>
+              <p>birthdate: {child.birthdate}</p>
+              <p>points available: {child.points_available}</p>
+              <p>money banked: ${child.money_banked}</p>
             </div>
             <br />
-            <div>
-              <h4>Tuesday</h4>
-              {child.tuesday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
-            </div>
-            <br />
-            <div>
-              <h4>Wednesday</h4>
-              {child.wednesday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
-            </div>
-            <br />
-            <div>
-              <h4>Thursday</h4>
-              {child.thursday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
-            </div>
-            <br />
-            <div>
-              <h4>Friday</h4>
-              {child.friday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
-            </div>
-            <br />
-            <div>
-              <h4>Saturday</h4>
-              {child.saturday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
-            </div>
-            <br />
-            <div>
-              <h4>Sunday</h4>
-              {child.sunday_chores.map(chore => (
-                <div key={chore.id}>
-                <p>{chore.title}</p>
-                </div>
-              ))}
+            <div style={{ display:"flex", flexDirection:"row"}}>
+              <div>
+                <h4>Monday</h4>
+                {child.monday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <input type="checkbox" /> {chore.title}
+                  </div>
+                ))}
+              </div>
+              <br />
+              <div>
+                <h4>Tuesday</h4>
+                {child.tuesday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <p>{chore.title}</p>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <div>
+                <h4>Wednesday</h4>
+                {child.wednesday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <p>{chore.title}</p>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <div>
+                <h4>Thursday</h4>
+                {child.thursday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <p>{chore.title}</p>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <div>
+                <h4>Friday</h4>
+                {child.friday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <p>{chore.title}</p>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <div>
+                <h4>Saturday</h4>
+                {child.saturday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <p>{chore.title}</p>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <div>
+                <h4>Sunday</h4>
+                {child.sunday_chores.map(chore => (
+                  <div key={chore.id}>
+                  <p>{chore.title}</p>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         ))}
